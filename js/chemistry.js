aafe5b7d52a4d6d8a52959871425e0bc1bbd0a37
AproGGG <143408932+AproGGG@users.noreply.github.com>
2023-11-26 20:31:09 +0400

    Update chemistry.js

--- js/chemistry.js
+++ js/chemistry.js
@@ -45,7 +45,7 @@ const createBlog = (blog) => {
 
     if(data.subject=="Chemistry"){
         blogSection.innerHTML += `
-        <div class="blog-card" style="width: 600px">
+        <div class="blog-card" style="width: 100%">
             <img src="${data.bannerImage}" class="blog-image" alt="">
             <h1 class="blog-title" style="word-wrap: break-word">${data.title.substring(0, 60)}</h1>
             <p class="blog-overview" style="word-wrap: break-word">${data.article.substring(0, 100) + '...'}</p>
